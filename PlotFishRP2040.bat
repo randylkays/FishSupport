a3ab3e5d51944cbbe76274a07b90d37d33473ea6
randylkays <randylkays@gmail.com>
2024-02-04 16:28:41 -0800

    Update PlotFishRP2040.bat
    added commment

--- PlotFishRP2040.bat
+++ PlotFishRP2040.bat
@@ -1,3 +1,4 @@
 d:
+:: the next line maybe not be required because 'mpSRU0.bat' maybe run 'FishSerialCmdAndRead.py' from 'Fish_support\Gihub'
 cd D:\PythonPrograms\Pico3\GitHub\Serial_Read_Usart0
 python D:\PythonPrograms\Fish_support\Github\PlotFishRP2040.py
